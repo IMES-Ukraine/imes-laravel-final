cdd23a371437f07c6397d1cecb82bdb38209138c
elgveni <elgveni@gmail.com>
2021-11-15 09:59:22 +0200

    Card list add buttons

--- resources/js/components/CardList.vue
+++ resources/js/components/CardList.vue
@@ -27,6 +27,10 @@
         </template>
 
         <div class="main-db">
+            <div class="links_wrap right mb20">
+                <a class="sidebar_nav-button width-auto height-35" href="/banner/new"><span>Баннер</span></a>
+                <a class="sidebar_nav-button width-auto height-35 active" @click="addCard()"><span>Создать карточку</span></a>
+            </div>
             <div class="db__block">
                 <div class="db-edit card">
                     <div class="card-body">
