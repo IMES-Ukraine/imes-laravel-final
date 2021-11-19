c95eb411d835bd4cf0933c152a2811e3c9ed073c
elgveni <elgveni@gmail.com>
2021-11-19 11:57:06 +0200

    Dashboard change

--- app/Services/PassingService.php
+++ app/Services/PassingService.php
@@ -2,7 +2,9 @@
 namespace App\Services;
 
 
+use App\Models\Articles;
 use App\Models\Passing;
+use App\Models\TestQuestions;
 
 class PassingService
 {
@@ -28,6 +30,34 @@ class PassingService
         return Passing::where(['entity_type' => $entity_type])
             ->where(['entity_id' => $entity_id])
             ->get();
+    }
+
+    /**
+     * @param $content_id
+     * @return mixed
+     */
+    public static function getPassingTotal($content_id) {
+        $articles_ids = ArticleService::pluckIDArticles($content_id);
+        $test_ids = TestService::pluckIDArticles($content_id);
+
+        return Passing::whereRaw('`entity_type` = "TestQuestions" AND `entity_id` IN(' . implode(",", $test_ids) . ')')
+            ->orWhereRaw('`entity_type` = "Post" AND `entity_id` IN(' . implode(",", $articles_ids) . ')')
+            ->count();
+    }
+
+    /**
+     * @param $content_id
+     * @param $status
+     * @return mixed
+     */
+    public static function getPassingTotalStatus($content_id, $status) {
+        $articles_ids = ArticleService::pluckIDArticles($content_id);
+        $test_ids = TestService::pluckIDArticles($content_id);
+
+        return Passing::where('status', $status)
+            ->whereRaw('`entity_type` = "TestQuestions" AND `entity_id` IN(' . implode(",", $test_ids) . ')')
+            ->orWhereRaw('`entity_type` = "Post" AND `entity_id` IN(' . implode(",", $articles_ids) . ')')
+            ->count();
     }
 
 }
