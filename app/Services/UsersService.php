19834b0636bdb8f4821479f7e5c5ef85b74cca61
p-clerick <devnextdoorcoders@gmail.com>
2022-01-14 14:56:27 +0200

    fix dashboard

--- app/Services/UsersService.php
+++ app/Services/UsersService.php
@@ -2,20 +2,11 @@
 
 namespace App\Services;
 
-use App\Models\Notifications;
 use App\Models\User;
-use App\Traits\NotificationsHelper;
+
 
 class UsersService
 {
-    public static function setBalance($user_id, $balance)
-    {
-        $user = User::find($user_id);
-        $user->balance = $user->balance + $balance;
-        $user->save();
-
-    }
-
     public static function getBalance($user_id)
     {
         $user = User::find($user_id);
